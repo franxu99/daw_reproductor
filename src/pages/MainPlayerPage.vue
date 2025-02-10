ed659ccfad42e690b148793d4773ad580db40f78
fran-bedmar-garsa <110120388+franxu99@users.noreply.github.com>
2025-02-10 21:16:15 +0100

    Scroll infinito

--- src/pages/MainPlayerPage.vue
+++ src/pages/MainPlayerPage.vue
@@ -1,16 +1,14 @@
 <template>
-    <div class="main-player-container">
+    <div class="main-player-container" @scroll="handleScroll">
         <div v-show="searchDataStore.loadSearch == true">
-
-            <div v-for="(soundData, key) in soundsData" class="song-element">
+            <div v-for="(soundData, key) in soundsData" :key="key" class="song-element">
                 <div class="song-img">
                     <img :src="soundData.images ? soundData.images.spectral_m : ''">
                 </div>
                 <div class="song-info">
                     <div class="song-info-name">
-                        <label>{{  soundData.name }}</label>
+                        <label>{{ soundData.name }}</label>
                     </div>
-
                     <div class="song-info-data">
                         <label>{{ soundData.username }}</label>
                         <label>{{ soundData.duration }}s</label>
@@ -21,75 +19,85 @@
                     <i class="bi bi-info-circle btn-song-info"></i>
                     <i class="bi bi-play-circle btn-song-play" @click="changeCurrentSong(key)"></i>
                 </div>
-
             </div> 
-
         </div>
 
-        <div class="loading-gif-container" v-show="searchDataStore.loadSearch == false">
+        <div class="loading-gif-container" v-show="!searchDataStore.loadSearch">
             <loadingComponent></loadingComponent>
         </div>
     </div>
 </template>
 
 <script setup>
+import { onMounted, ref, watch } from 'vue';
+import { useSoundDataStore } from '../stores/soundData';
+import { useSearchStore } from '@/stores/search';
+import loadingComponent from '@/components/loadingComponent.vue';
+import apiService from '../services/apiService';
 
-    import { onMounted, ref, watch } from 'vue';
-    import { useSoundDataStore } from '../stores/soundData';
-    import { useSearchStore } from '@/stores/search';
-    import loadingComponent from '@/components/loadingComponent.vue';
-    import apiService from '../services/apiService';
+const searchDataStore = useSearchStore();
+const soundDataStore = useSoundDataStore();
 
-    const searchDataStore = useSearchStore();
-    const soundDataStore = useSoundDataStore();
+let soundsData = ref([]);
+let nextPageUrl = ref(null);
+let isFetching = ref(false);
 
-    let soundsData = ref({});
-    let timeOutId = 0;
-
-
-    watch(
-        () => searchDataStore.searchText, 
-        () => {
-
-            searchDataStore.loadSearch = false;
-
-            if(timeOutId != 0){
-                clearTimeout(timeOutId)
-                timeOutId = 0;
-            }
-
-            timeOutId = setTimeout(getSoundsData, 1000);
-        }
-    );
-
-
-    onMounted(async () => {  
+watch(
+    () => searchDataStore.searchText, 
+    () => {
+        searchDataStore.loadSearch = false;
         getSoundsData();
-    });
-
-
-    const getSoundsData = async () => {
-        let response = await apiService.getSounds(searchDataStore.searchText);
-
-        soundsData.value = response.results;
-
-        soundsData.value.forEach(async (element, key) => {
-            let data = await apiService.getsoundData(element.id);
-
-            soundsData.value[key] = {
-                ...data,
-                ...soundsData.value[key]
-            };
-        });
-
-        timeOutId = 0;
-        searchDataStore.loadSearch = true;
     }
+);
 
+onMounted(() => {  
+    getSoundsData();
+});
+
+const getSoundsData = async () => {
+    if (isFetching.value) return;
+    isFetching.value = true;
+
+    let response = await apiService.getSounds(searchDataStore.searchText);
     
-    const changeCurrentSong = (key) => {
-        soundDataStore.soundName = soundsData.value[key].name;
-        soundDataStore.soundImg = soundsData.value[key].images.spectral_m;
-        soundDataStore.soundUrl = soundsData.value[key].previews["preview-hq-mp3"];
+    soundsData.value = response.results;
+    nextPageUrl.value = response.next; // Guardamos la URL de la siguiente página
+
+    await fetchSoundDetails();
+    searchDataStore.loadSearch = true;
+    isFetching.value = false;
+};
+
+const loadMoreSounds = async () => {
+    if (!nextPageUrl.value || isFetching.value) return;
+    isFetching.value = true;
+
+    let response = await apiService.getNextPage(nextPageUrl.value);
+    
+    soundsData.value.push(...response.results);
+    nextPageUrl.value = response.next;
+
+    await fetchSoundDetails();
+    isFetching.value = false;
+};
+
+const fetchSoundDetails = async () => {
+    for (let key in soundsData.value) {
+        let data = await apiService.getsoundData(soundsData.value[key].id);
+        soundsData.value[key] = { ...data, ...soundsData.value[key] };
     }
+};
+
+const handleScroll = (event) => {
+    let { scrollTop, scrollHeight, clientHeight } = event.target;
+    if (scrollTop + clientHeight >= scrollHeight - 50) { 
+        loadMoreSounds();
+    }
+};
+
+const changeCurrentSong = (key) => {
+    soundDataStore.soundName = soundsData.value[key].name;
+    soundDataStore.soundImg = soundsData.value[key].images.spectral_m;
+    soundDataStore.soundUrl = soundsData.value[key].previews["preview-hq-mp3"];
+};
 </script>
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,24 +1,22 @@
 export default {
-
     BASE_URL: "https://freesound.org/apiv2",
     TOKEN: "94QI2cQexWIEFtOEIBCoQOUja0rKDf0N3JXFuaGp",
 
     async get(url) {
-        url = url + "&token=" + this.TOKEN;
-
-        return await fetch(url);
+        url = url.includes("token=") ? url : url + "&token=" + this.TOKEN;
+        let response = await fetch(url);
+        return response.json();
     },
 
-    async getSounds(searchQuery = ""){
-        let response = (await this.get(this.BASE_URL + "/search/text/?query=" + searchQuery)).json();   
-        
-        return response;
+    async getSounds(searchQuery = "") {
+        return this.get(`${this.BASE_URL}/search/text/?query=${searchQuery}`);
     },
 
-    async getsoundData(id){
-        let response = (await this.get(this.BASE_URL + "/sounds/" + id +"/?")).json();   
-        
-        return response;
+    async getsoundData(id) {
+        return this.get(`${this.BASE_URL}/sounds/${id}/?`);
+    },
+
+    async getNextPage(url) {
+        return this.get(url);
     }
-
-}
+};
